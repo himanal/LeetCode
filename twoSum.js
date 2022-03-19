72f7528569b50b8b76d00bfe2fe986245bd3fa92
himanal <almassi.yazdan@gmail.com>
2022-03-19 08:30:56 +0330

    two sum

---
+++ twoSum.js
@@ -0,0 +1,10 @@
+var twoSum = function(nums, target) {
+        for (let i = 0; i < nums.length; i++) {
+            for (let j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return [nums[i], nums[j]]
+                }
+            }
+        }
+};
+console.log(twoSum([1,3,2,4],6))
