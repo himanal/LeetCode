ee783538c7aa4a5a2d03f9f695902ccd9379a5a7
himanal <almassi.yazdan@gmail.com>
2022-03-20 09:43:38 +0330

    Search Insert Position

---
+++ SearchInsertPosition.js
@@ -0,0 +1,16 @@
+
+
+
+var searchInsert = function(nums, target) {
+
+    for (let i = 0; i < nums.length; i++) {
+       
+        if (nums[i] >= target) {
+            return i;
+        };
+    };
+    return nums.length
+    
+};
+
+console.log( searchInsert([1,2,3,4,5,8],7))
