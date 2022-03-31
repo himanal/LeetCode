c892ff9ccf794249fe53fc965c8be7ea26768f2d
himanal <almassi.yazdan@gmail.com>
2022-03-31 18:56:07 +0430

    Longest Substring Without Repeating Characters

---
+++ LongestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,14 @@
+var lengthOfLongestSubstring = function(s) {
+
+    const map = {};
+    var left = 0;
+    
+    return s.split('').reduce((max, v, i) => {
+        left = map[v] >= left ? map[v] + 1 : left;
+        map[v] = i;
+        return Math.max(max, i - left + 1);
+    }, 0);
+    
+};
+
+console.log(lengthOfLongestSubstring("abcabcdbb"))
