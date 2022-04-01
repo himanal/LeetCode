2398e12333a4917cad8899c5105456a9b4048583
himanal <almassi.yazdan@gmail.com>
2022-04-01 20:21:15 +0430

    Subarrays with K Different Integers

--- LongestSubstringWithoutRepeatingCharacters.js
+++ LongestSubstringWithoutRepeatingCharacters.js
@@ -2,13 +2,11 @@ var lengthOfLongestSubstring = function(s) {
 
     const map = {};
     var left = 0;
-    
     return s.split('').reduce((max, v, i) => {
         left = map[v] >= left ? map[v] + 1 : left;
         map[v] = i;
         return Math.max(max, i - left + 1);
     }, 0);
-    
 };
 
 console.log(lengthOfLongestSubstring("abcabcdbb"))
